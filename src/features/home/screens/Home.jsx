81f586026d8d4a88741d038e2b99491b0e73a071
DaniDuran09 <daniel.duran.torres09@gmail.com>
2026-02-19 22:15:16 -0600

    feat: home screen and styles

--- src/features/home/screens/Home.jsx
+++ src/features/home/screens/Home.jsx
@@ -1,6 +1,8 @@
 import { IoMdNotificationsOutline } from "react-icons/io";
 import styles from './Home.module.css';
 import { FiPlus } from "react-icons/fi";
+import Building_04 from '../../../assets/icons/Building_04.svg';
+import { Colors } from "../../../assets/Colors";
 
 const options = [
     {
@@ -62,7 +64,7 @@ function Home() {
             <div className={styles.statsGrid}>
                 {options.map((option, index) => (
                     <div key={index} className={styles.statCard}>
-                        <h3>{option.name}</h3>
+                        <h5>{option.name}</h5>
                         <h1 style={{ fontSize: '36px', margin: '10px 0' }}>{Number(option.number).toFixed(1)}</h1>
                         <p style={{
                             color: option.type === 'positive' ? 'green' : 'red',
@@ -72,7 +74,66 @@ function Home() {
                     </div>
                 ))}
             </div>
+            <div
+                className={styles.bottomSection}>
+                {/*SOLICITUDES PENDIENTES */}
+                <div style={{ flex: 2, height: '100%', backgroundColor: 'white', borderRadius: '10px', border: '1px solid #ddd', padding: '10px' }}>
+                    <div className={styles.ResponsiveTitleContainer}>
+                        <h3 style={{}}>Solicitudes pendientes {'>'}</h3>
+                    </div>
+                    {options.map((option, index) => (
+                        <div key={index} className={styles.statCard}>
+                            <h3>{option.name}</h3>
+                            <h1 style={{ fontSize: '36px', margin: '10px 0' }}>{Number(option.number).toFixed(1)}</h1>
+                            <p style={{
+                                color: option.type === 'positive' ? 'green' : 'red',
+                                fontWeight: 'bold',
+                                fontSize: '0.9rem'
+                            }} >{option.stats}</p>
+                        </div>
+                    ))}
 
+                </div>
+                <div className={styles.quickActionsContainer} >
+                    <h3>Acciones rapidas</h3>
+                    {/*AQUI VAN LAS SOLICITUDES PENDIENTES*/}
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }} >
+                        <button className={styles.quickActionButton} style={{ backgroundColor: Colors.primaryColor }} >
+                            <div style={{ flex: 1, display: 'flex', justifyContent: 'left' }}  >
+                                <img src={Building_04} alt="" />
+                            </div>
+                            <div style={{ flex: 1, flexDirection: 'column' }} >
+                                <h2 style={{ display: 'flex', justifyContent: 'left' }} >Registrar Espacio</h2>
+                                <h2 style={{ display: 'flex', justifyContent: 'left' }} >Agregar un nuevo espacio</h2>
+
+                            </div>
+                        </button>
+
+                        <button className={styles.quickActionButton} style={{ backgroundColor: '#D4F1E8' }} >
+                            <div style={{ flex: 1, display: 'flex', justifyContent: 'left' }}  >
+                                <img src={Building_04} alt="" />
+                            </div>
+                            <div style={{ flex: 1, flexDirection: 'column' }} >
+                                <h2 style={{ display: 'flex', justifyContent: 'left' }} >Registrar Espacio</h2>
+                                <h2 style={{ display: 'flex', justifyContent: 'left' }} >Agregar un nuevo espacio</h2>
+
+                            </div>
+                        </button>
+
+                        <button className={styles.quickActionButton} style={{ backgroundColor: '#FFF4D4' }} >
+                            <div style={{ flex: 1, display: 'flex', justifyContent: 'left' }}  >
+                                <img src={Building_04} alt="" />
+                            </div>
+                            <div style={{ flex: 1, flexDirection: 'column' }} >
+                                <h2 style={{ display: 'flex', justifyContent: 'left' }} >Registrar Espacio</h2>
+                                <h2 style={{ display: 'flex', justifyContent: 'left' }} >Agregar un nuevo espacio</h2>
+
+                            </div>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
