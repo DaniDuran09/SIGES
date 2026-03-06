050d33e2f7257c02804af1682e081df9db1935d4
DaniDuran09 <daniel.duran.torres09@gmail.com>
2026-03-06 16:53:53 -0600

    feat: change pending request styles and structure

--- src/features/home/components/PendingRequestComponent.jsx
+++ src/features/home/components/PendingRequestComponent.jsx
@@ -11,7 +11,8 @@ function PendingRequestComponent({ props }) {
         <div className={styles.pendingRequestCard}>
             <p className={styles.applierName}>{ApplierName}</p>
             <div className={styles.infoContainer}>
-                <h1 className={styles.objectName}>{objectName}</h1>
+                <p className={styles.objectName}>{objectName}</p>
+                <p className={styles.objectName} >{`*`}</p>
                 <p className={styles.date}>{date}</p>
             </div>
         </div>
