0a6d6bb0a64488f327a1b2cf806dc0bfa3c015f2
DaniDuran09 <daniel.duran.torres09@gmail.com>
2026-03-05 23:23:31 -0600

    add: functional space modal

--- src/features/spaces/components/NewSpaceModal.jsx
+++ src/features/spaces/components/NewSpaceModal.jsx
@@ -7,8 +7,17 @@ import { Alert } from '@mui/material';
 
 export const NewSpaceModal = ({ onClose }) => {
     const [showAddType, setShowAddType] = useState(false);
+    const [newTypeName, setNewTypeName] = useState("");
+    const [newTypeDescription, setNewTypeDescription] = useState("");
     const [showAddBuilding, setShowAddBuilding] = useState(false);
     const [newBuildingName, setNewBuildingName] = useState("");
+
+    const [spaceName, setSpaceName] = useState("");
+    const [spaceType, setSpaceType] = useState("");
+    const [spaceBuilding, setSpaceBuilding] = useState("");
+    const [spaceCapacity, setSpaceCapacity] = useState("");
+    const [spaceDescription, setSpaceDescription] = useState("");
+    const [spaceRestricted, setSpaceRestricted] = useState(true);
 
     const queryClient = useQueryClient();
 
@@ -19,14 +28,35 @@ export const NewSpaceModal = ({ onClose }) => {
         })
     })
 
-
-
     const { data: b_buildings, isLoading: b_buildingsLoading, error: b_buildingsError } = useQuery({
         queryKey: ["getBuildings"],
         queryFn: () => apiFetch("/buildings", {
             method: "GET",
         })
     })
+
+    const mutationType = useMutation({
+        mutationFn: (data) => apiFetch("/spacetypes", {
+            method: "POST",
+            body: JSON.stringify(data),
+        }),
+        onSuccess: () => {
+            setShowAddType(false)
+            setNewTypeName("")
+            setNewTypeDescription("")
+            queryClient.invalidateQueries({ queryKey: ["getSpaceTypes"] });
+        },
+        onError: (error) => {
+            console.log("Error", error.message);
+        }
+    })
+
+    const handleAddType = () => {
+        mutationType.mutate({
+            name: newTypeName,
+            description: newTypeDescription,
+        })
+    }
 
     const mutationBuilding = useMutation({
         mutationFn: (data) => apiFetch("/buildings", {
@@ -48,7 +78,44 @@ export const NewSpaceModal = ({ onClose }) => {
         })
     }
 
+    const mutationSpace = useMutation({
+        mutationFn: (data) => apiFetch("/spaces", {
+            method: "POST",
+            body: JSON.stringify(data),
+        }),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ["getSpaces"] });
+            onClose();
+            return (
+                <Alert severity="success">
+                    Espacio registrado exitosamente
+                </Alert>
+            )
+        },
+        onError: (error) => {
+            return (
+                <Alert severity="error">
+                    Error al registrar el espacio
+                </Alert>
+            )
+        }
+    });
 
+    const handleAddSpace = () => {
+
+        mutationSpace.mutate({
+            status: "AVAILABLE",
+            name: spaceName,
+            description: spaceDescription,
+            studentsAvailable: !spaceRestricted,
+            buildingId: parseInt(spaceBuilding) || 0,
+            availability: [],
+            exceptions: [],
+            spaceTypeId: parseInt(spaceType) || 0,
+            bookInAdvanceDuration: "PT24H",
+            capacity: parseInt(spaceCapacity) || 0
+        });
+    };
 
     return (
         <div className={styles.overlay}>
@@ -66,56 +133,89 @@ export const NewSpaceModal = ({ onClose }) => {
 
                     <div className={styles.formGroup}>
                         <label>Nombre del espacio</label>
-                        <input type="text" placeholder="Ej. Auditorio Principal" />
+                        <input type="text" placeholder="Ej. Auditorio Principal" value={spaceName} onChange={(e) => setSpaceName(e.target.value)} />
                     </div>
 
                     <div className={styles.formGroup}>
                         <label>Tipo</label>
                         <div className={styles.selectWrapper}>
-                            <select defaultValue="">
+                            <select value={spaceType} onChange={(e) => {
+                                if (e.target.value === "add_type") {
+                                    setShowAddType(true);
+                                } else {
+                                    setSpaceType(e.target.value);
+                                    setShowAddType(false);
+                                }
+                            }}>
                                 <option value="" disabled>Seleccione...</option>
                                 {b_types?.map((type) => (
                                     <option key={type.id} value={type.id}>
                                         {type.name}
                                     </option>
                                 ))}
-                                <option value="">Agregar tipo</option>
+                                <option value="add_type">Agregar tipo</option>
                             </select>
                         </div>
+                        {showAddType && (
+                            <div className={styles.addSubForm}>
+                                <span className={styles.addSubFormTitle}>Nuevo tipo</span>
+                                <div className={styles.addSubFormCol}>
+                                    <input type="text" placeholder="Ej. Aula" value={newTypeName} onChange={(e) => setNewTypeName(e.target.value)} />
+                                    <input type="text" placeholder="Ej. Salón de clases útil para presentaciones..." value={newTypeDescription} onChange={(e) => setNewTypeDescription(e.target.value)} />
+                                    <div className={styles.addSubFormActions}>
+                                        <button className={styles.btnAdd} onClick={handleAddType} disabled={mutationType.isPending}>
+                                            {mutationType.isPending ? "Cargando..." : "Agregar"}
+                                        </button>
+                                        <button className={styles.btnDismiss} onClick={() => { setShowAddType(false); setNewTypeName(""); setNewTypeDescription(""); }}>X</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
 
                     <div className={styles.formRow}>
                         <div className={styles.formGroup}>
                             <label>Ubicación</label>
-                            <select>
-                                <option onClick={() => { setShowAddBuilding(false) }} value="">Seleccione...</option>
+                            <select value={spaceBuilding} onChange={(e) => {
+                                if (e.target.value === "add_building") {
+                                    setShowAddBuilding(true);
+                                } else {
+                                    setSpaceBuilding(e.target.value);
+                                    setShowAddBuilding(false);
+                                }
+                            }}>
+                                <option value="" disabled>Seleccione...</option>
                                 {b_buildings?.map((building) => (
-                                    <option onClick={() => { setShowAddBuilding(false) }} key={building.id} value={building.id}>
+                                    <option key={building.id} value={building.id}>
                                         {building.name}
                                     </option>
                                 ))}
-                                <option onClick={() => { setShowAddBuilding(true) }} value="">Agregar ubicación</option>
+                                <option value="add_building">Agregar ubicación</option>
                             </select>
                             {showAddBuilding && (
-                                <div style={{ marginTop: "10px", backgroundColor: "#D8D3E4", padding: "10px", borderRadius: "5px" }} className={styles.formGroup}>
-                                    <label  >Nueva ubicación</label>
-                                    <div style={{ display: "flex", gap: "10px", marginTop: "10px" }}>
-                                        <input style={{ backgroundColor: "#fff", color: "#000" }} type="text" placeholder="Ej. Edificio A" value={newBuildingName} onChange={(e) => setNewBuildingName(e.target.value)} />
-                                        <button style={{ backgroundColor: "#D4F1E8", color: "#1aa76fff", padding: "5px 10px", borderRadius: "5px" }} onClick={handleAddBuilding}>Agregar</button>
-                                        <button style={{ backgroundColor: "#fff", color: "#000000ff", padding: "5px 25px", borderRadius: "5px" }} onClick={() => { setShowAddBuilding(false) }} >X</button>
+                                <div className={styles.addSubForm}>
+                                    <span className={styles.addSubFormTitle}>Nueva ubicación</span>
+                                    <div className={styles.addSubFormCol}>
+                                        <input type="text" placeholder="Ej. Edificio A" value={newBuildingName} onChange={(e) => setNewBuildingName(e.target.value)} />
+                                        <div className={styles.addSubFormActions}>
+                                            <button className={styles.btnAdd} onClick={handleAddBuilding} disabled={mutationBuilding.isPending}>
+                                                {mutationBuilding.isPending ? "Cargando..." : "Agregar"}
+                                            </button>
+                                            <button className={styles.btnDismiss} onClick={() => { setShowAddBuilding(false); setNewBuildingName(""); }}>X</button>
+                                        </div>
                                     </div>
                                 </div>
                             )}
                         </div>
                         <div className={styles.formGroup}>
                             <label>Capacidad</label>
-                            <input type="number" placeholder="Ej. 200" />
+                            <input type="number" placeholder="Ej. 200" value={spaceCapacity} onChange={(e) => setSpaceCapacity(e.target.value)} />
                         </div>
                     </div>
 
                     <div className={styles.formGroup}>
                         <label>Descripción</label>
-                        <textarea placeholder="Descripción del espacio..."></textarea>
+                        <textarea placeholder="Descripción del espacio..." value={spaceDescription} onChange={(e) => setSpaceDescription(e.target.value)}></textarea>
                     </div>
 
                     <div className={styles.formGroup}>
@@ -123,7 +223,7 @@ export const NewSpaceModal = ({ onClose }) => {
                         <div className={styles.toggleContainer}>
                             <span>Restringido para estudiantes</span>
                             <label className={styles.switch}>
-                                <input type="checkbox" defaultChecked />
+                                <input type="checkbox" checked={spaceRestricted} onChange={(e) => setSpaceRestricted(e.target.checked)} />
                                 <span className={styles.slider}></span>
                             </label>
                         </div>
@@ -132,11 +232,15 @@ export const NewSpaceModal = ({ onClose }) => {
                 </div>
 
                 <div className={styles.footer}>
-                    <button className={styles.cancelButton} onClick={onClose}>
+                    <button className={styles.cancelButton} onClick={onClose} disabled={mutationSpace.isPending}>
                         Cancelar
                     </button>
-                    <button className={styles.submitButton}>
-                        Registrar Espacio
+                    <button
+                        className={styles.submitButton}
+                        onClick={handleAddSpace}
+                        disabled={mutationSpace.isPending}
+                    >
+                        {mutationSpace.isPending ? "Registrando..." : "Registrar Espacio"}
                     </button>
                 </div>
 
